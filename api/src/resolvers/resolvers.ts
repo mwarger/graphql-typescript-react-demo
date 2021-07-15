d956e91ce226d0d970aee16a64b9c3276b0f4741
Mat Warger <mwarger@gmail.com>
2021-07-15 08:52:56 -0500

    added hello world back

--- api/src/resolvers/resolvers.ts
+++ api/src/resolvers/resolvers.ts
@@ -11,6 +11,7 @@ const resolvers: {
 } = {
   Query: {
     // TODO: what possible resolvers exist?
+    hello: () => 'world',
 
     nowPlaying(_parent, _args, context, _info) {
       return context.dataSources.movieDataSource.nowPlaying();
